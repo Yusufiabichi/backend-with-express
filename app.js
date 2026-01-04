3af04a1a30514cdefc6e2e47f0cafdfdff336fb2
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-04 07:40:13 +0100

    sec commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send("Hello Express");
+// });
+
+app.get('/', (req, res) => {
+    res.send([1, 2, 3, 4, 5]);
+});
+
+//Routes
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}...`)
+})
