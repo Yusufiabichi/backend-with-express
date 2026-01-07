58ddd7789cbcfca9d21a6eda5dbe15b3239d2904
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-07 07:28:13 +0100

    post req.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //     res.send("Hello Express");
 // });
@@ -9,15 +11,22 @@ const app = express();
 const courses = [
     {id:1, name: 'course1'},
     {id:2, name: 'course2'},
-]
+    {id:3, name: 'course3'}
+];
 app.get('/', (req, res) => {
-    res.send([1, 2, 3, 4, 5]);
+    res.send([1, 3, 3, 4, 5]);
 });
 
 //Routes Parameters
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course)res.status(404).send('The course not found');
+    res.send(course);
 });
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.get('/api/blogs/:id', (req, res) => {
     res.send(req.params.id)
@@ -32,6 +41,21 @@ app.get('/api/posts/:year/:month', (req, res) => {
 app.get('/api/posts/:year/:month', (req, res) => {
     res.send(req.query);
 });
+
+// Post Request
+app.post('/api/courses', (req, res) =>{
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course)res.status(404).send('The course not found');
+})
 
 
 const port = process.env.PORT || 3000;
