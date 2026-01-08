2ef7732a629d01c1e397d8947ccf69bac83e6f87
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-08 16:23:09 +0100

    Joi validation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+const Joi = require('joi')
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
@@ -44,6 +44,18 @@ app.get('/api/posts/:year/:month', (req, res) => {
 
 // Post Request
 app.post('/api/courses', (req, res) =>{
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+
+    const result = schema.validate(req.body);
+    console.log(result)
+
+    if(!req.body.name || req.body.name.length < 3) {
+        res.status(400).send("Name is required and should be minimum of 3 characters");
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -58,7 +70,7 @@ app.put('api/courses/:id', (req, res) => {
 })
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}...`)
