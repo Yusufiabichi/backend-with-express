060bcba9551b332f0979f8de9c264a1b7aa0cbd1
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-15 08:06:08 +0100

    Put Request

--- app.js
+++ app.js
@@ -48,15 +48,11 @@ app.get('/api/students/:id', (req, res) => {
 
 // Post Request
 app.post('/api/courses', (req, res) =>{
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
 
-    const result = schema.validate(req.body);
-    console.log(result)
-
-    if(!req.body.name || req.body.name.length < 3) {
-        res.status(400).send("Name is required and should be minimum of 3 characters");
+    //Joi Validation
+    const {error} = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
         return;
     }
 
@@ -68,10 +64,31 @@ app.post('/api/courses', (req, res) =>{
     res.send(course);
 });
 
-app.put('api/courses/:id', (req, res) => {
+
+
+app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course)res.status(404).send('The course not found');
-})
+    if(!course)res.status(404).send('The course with the given ID was not found');
+    
+    // const result = Joi.validate(req.body, schema);
+    // const result = validateCourse(req.body);
+    const {error} = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    //Update course
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(course);
+}
 
 
 const port = process.env.PORT || 5000;
