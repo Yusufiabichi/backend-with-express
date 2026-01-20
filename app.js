26b87d549f31dbab7e7d6b15e885d35991cf306d
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-20 12:26:28 +0100

    genre post req.

--- app.js
+++ app.js
@@ -118,6 +118,16 @@ app.get('/api/genres/:id', (req, res)=> {
     res.send(genre);
 })
 
+app.post('/api/genres/', (req, res)=>{
+    const { error } = validateGenre(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    }
+})
+
 app.put('/api/genres/:id', (req, res) => {
     const genre = genres.find(g => g.id === parseInt(req.params.id));
     if(!genre) return res.status(404).send('The genre with the given ID was not found');
