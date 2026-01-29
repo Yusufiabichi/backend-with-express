c682a43212fe80ca729688502b0160e41c523e7c
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-29 14:31:37 +0100

    consuming promise

--- async/promise.js
+++ async/promise.js
@@ -1,10 +1,25 @@
-const p = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(1);
-        reject(new Error('message'));
-    }, 2000);
-});
+// const p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(1);
+//         reject(new Error('message'));
+//     }, 2000);
+// });
 
-p
-    .then(result => console.log('Result', result))
-    .catch(err => console.log('Error', err.message));
+// p
+//     .then(result => console.log('Result', result))
+//     .catch(err => console.log('Error', err.message));
+
+
+    getUser(1, (user) => {
+        getRepositories(user.gitHubUsername, (repos) => {
+            getCommits(repos[0], (commits) => {
+                console.log(commits);
+            })
+        })
+    });
+
+    getUser(1)
+        .then(user => getRepositories(user.gitHubUsername))
+        .then(repos => getCommits(repos[0]))
+        .then(commits => console.log('Commits', commits))
+        .catch(err => console.log('Error', err.message))
