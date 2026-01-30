2e8040bbf843c64e1d3251575dd6955641fcafe0
Yusufiabichi <yusufiabichi@yahoo.com>
2026-01-30 06:48:15 +0100

    parallel promises

--- async/promise.js
+++ async/promise.js
@@ -10,16 +10,36 @@
 //     .catch(err => console.log('Error', err.message));
 
 
-    getUser(1, (user) => {
-        getRepositories(user.gitHubUsername, (repos) => {
-            getCommits(repos[0], (commits) => {
-                console.log(commits);
-            })
-        })
+    // getUser(1, (user) => {
+    //     getRepositories(user.gitHubUsername, (repos) => {
+    //         getCommits(repos[0], (commits) => {
+    //             console.log(commits);
+    //         })
+    //     })
+    // });
+
+    // getUser(1)
+    //     .then(user => getRepositories(user.gitHubUsername))
+    //     .then(repos => getCommits(repos[0]))
+    //     .then(commits => console.log('Commits', commits))
+    //     .catch(err => console.log('Error', err.message))
+
+
+    const p1 = new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('Async op 1...')
+            resolve(1);
+        }, 2000);
     });
 
-    getUser(1)
-        .then(user => getRepositories(user.gitHubUsername))
-        .then(repos => getCommits(repos[0]))
-        .then(commits => console.log('Commits', commits))
-        .catch(err => console.log('Error', err.message))
+    const p2 = new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('Async op 2...')
+            resolve(2);
+        }, 2000);
+    });
+
+
+    Promise.race([p1, p2])
+        .then(result => console.log(result))
+        .catch(err => console.log('Error', err.message));
