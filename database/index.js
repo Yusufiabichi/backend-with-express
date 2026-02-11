72e0123b9c263620e94dcef326355900f7ec3712
Yusufiabichi <yusufiabichi@yahoo.com>
2026-02-11 21:50:59 +0100

    create course function

--- database/index.js
+++ database/index.js
@@ -4,26 +4,26 @@ mongoose.connect('mongodb://localhost:27017/')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// const courseSchema = new mongoose.Schema({
-//     name: String,
-//     author: String,
-//     tags: [String],
-//     date: {type: Date, default: Date.now},
-//     isPublished: Boolean
-// });
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
 
-// const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model('Course', courseSchema);
 
-// async function createCourse(){
-//     const course = new Course({
-//         name: 'Nodejs course',
-//         author: 'Yusufiabichi',
-//         tags:['node', 'backend'],
-//         isPublished: true
-//     });
+async function createCourse(){
+    const course = new Course({
+        name: 'Nodejs course',
+        author: 'Yusufiabichi',
+        tags:['node', 'backend'],
+        isPublished: true
+    });
     
-//     const result = await course.save();
-//     console.log(result);
-// } 
+    const result = await course.save();
+    console.log(result);
+} 
 
-// createCourse()
+createCourse()
